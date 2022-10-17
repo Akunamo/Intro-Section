caa8e92110363ce1681c9fe7c1aec44d14e3a93a
Aviral <2knightsoldier@gmail.com>
2022-10-17 12:00:05 +0530

    getAttribute(attr) returns a string daamn it, so cant use ternary operator like attr?F:T had to use attr===?F:T, saw many ways to toggle something like using dataset or aria-label or classes phew, using closest for parent varification, var = null when element not found in DOM it gives null

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const toggleBtn = document.querySelector(".toggle-nav");
 const nav = document.querySelector("#navigation");
+
 toggleBtn.addEventListener("click", toggleNav);
+
+
 
 function toggleNav(e) {
   let expand = e.target.getAttribute("aria-expanded");
-//  get attribute gives a string so had to us double quotes at 8
+  //  get attribute gives a string so had to us double quotes at 8
   if (e.target.getAttribute("aria-expanded") === "false") {
     e.target.setAttribute("aria-expanded", true);
     nav.setAttribute("aria-visible", true);
@@ -15,4 +18,22 @@ function toggleNav(e) {
     e.target.setAttribute("aria-expanded", false);
   }
   console.log(this.getAttribute("aria-expanded"));
+}
+
+document.addEventListener('click', toggleDropDown);
+
+function toggleDropDown(e) {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]") 
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+  let currentDropdown = e.target.closest("[data-dropdown]");
+  const dropdownMenu = currentDropdown.querySelector(".dropdown");
+  
+  let toggle = dropdownMenu.getAttribute("aria-visible") === "false"? true : false;
+  // dropdownMenu.getAttribute("aria-visible") ? true : false WONT WORK
+  // getAttribute returns a string  so had to usee === "stringval"
+  // console.log(typeof toggle);
+  if (isDropdownButton){
+    dropdownMenu.setAttribute("aria-visible", toggle)
+  }
+
 }

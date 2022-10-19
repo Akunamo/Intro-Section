b9c47b923578214aa88d0ed7945e915e6485c5ec
Aviral <2knightsoldier@gmail.com>
2022-10-19 19:43:34 +0530

    dropdown complete

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ const nav = document.querySelector("#navigation");
 
 toggleBtn.addEventListener("click", toggleNav);
 
-
+const allDropdown = document.querySelectorAll("[data-dropdown");
+console.log(allDropdown);
 
 function toggleNav(e) {
   let expand = e.target.getAttribute("aria-expanded");
-  //  get attribute gives a string so had to us double quotes at 8
+  //  get attribute gives a string so had to us double quotes below
   if (e.target.getAttribute("aria-expanded") === "false") {
     e.target.setAttribute("aria-expanded", true);
     nav.setAttribute("aria-visible", true);
     // expand = true;
-    // here m changing var value not the attr value
+    // here i was changing var value not the attr value
   } else {
     nav.setAttribute("aria-visible", false);
     e.target.setAttribute("aria-expanded", false);
@@ -23,17 +24,27 @@ function toggleNav(e) {
 document.addEventListener('click', toggleDropDown);
 
 function toggleDropDown(e) {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]") 
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
   if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
-  let currentDropdown = e.target.closest("[data-dropdown]");
-  const dropdownMenu = currentDropdown.querySelector(".dropdown");
   
-  let toggle = dropdownMenu.getAttribute("aria-visible") === "false"? true : false;
-  // dropdownMenu.getAttribute("aria-visible") ? true : false WONT WORK
+  let dropdownMenu;
+  let currentDropdown;
+  // dropdownMenu.getAttribute("aria-visible") ? true : false WONT WORK (Again 8)
   // getAttribute returns a string  so had to usee === "stringval"
   // console.log(typeof toggle);
-  if (isDropdownButton){
-    dropdownMenu.setAttribute("aria-visible", toggle)
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    dropdownMenu = currentDropdown.querySelector(".dropdown");
+    let toggle = currentDropdown.getAttribute("aria-expanded") === "false" ? true : false;
+    dropdownMenu.setAttribute("aria-visible", toggle);
+    currentDropdown.setAttribute("aria-expanded", toggle)
+    console.log(currentDropdown);
   }
 
+  const dm = document.querySelectorAll("[data-dropdown]").forEach(dd =>{
+    if (dd !== currentDropdown) {
+      dd.querySelector(".dropdown").setAttribute("aria-visible", false);
+      dd.setAttribute("aria-expanded", false);
+    }
+  })
 }
